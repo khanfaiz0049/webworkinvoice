cd3c308d1a86d2c4fe801f5423060d076dd21b37
Faiz Khan <khanfaiz0049@email.com>
2026-05-22 17:37:40 +0530

    feat: set default login credentials in DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,9 +17,12 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        User::updateOrCreate(
+            ['email' => 'dharmesh@webwork.co.in'],
+            [
+                'name' => 'Dharmesh',
+                'password' => \Illuminate\Support\Facades\Hash::make('Mumbai@007'),
+            ]
+        );
     }
 }
